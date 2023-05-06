29201e42949dc55ebff749b98c07885d44788f6c
Sare Gaga <104974339+SareGaga@users.noreply.github.com>
2023-05-06 19:05:26 +0300

    Add files via upload

---
+++ dbo.Products.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Products] (
+    [Id]   INT NOT NULL,
+    [PizzaName] VARCHAR (20) NOT NULL,
+    [Ingredients]       VARCHAR(20)          NOT NULL, 
+    [Price] INT NOT NULL, 
+    CONSTRAINT [PK_Products] PRIMARY KEY ([Id])
+);
+
